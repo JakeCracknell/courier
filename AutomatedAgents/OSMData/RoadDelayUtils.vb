84d9ee4991d52fb46a2083e68102f57067a5b826
jc4512 <jc4512@imperial.ac.uk>
2015-04-19 18:12:14 +0100

    Timing bug fix with road delays

--- AutomatedAgents/OSMData/RoadDelayUtils.vb
+++ AutomatedAgents/OSMData/RoadDelayUtils.vb
@@ -9,7 +9,7 @@
         LEVEL_CROSSING
     End Enum
 
-    Private TrafficDistribution() As Integer = {24, 15, 10, 9, 10, 15, 24, 37, 55, 90, 129, 153, 158, 152, 148, 152, 156, 147, 129, 108, 84, 61, 41, 26, 16, 10, 8, 10, 20, 50, 114, 182, 185, 152, 147, 150, 150, 150, 154, 165, 189, 195, 147, 96, 67, 51, 37, 23, 15, 11, 10, 11, 19, 45, 112, 188, 193, 154, 139, 139, 140, 144, 152, 169, 199, 204, 157, 100, 68, 51, 39, 25, 16, 11, 10, 11, 19, 44, 110, 189, 195, 156, 141, 141, 143, 147, 156, 173, 203, 209, 163, 107, 72, 55, 41, 27, 16, 12, 10, 12, 19, 44, 109, 188, 195, 157, 145, 146, 149, 154, 163, 180, 208, 212, 169, 117, 81, 60, 45, 29, 18, 13, 11, 13, 19, 42, 99, 171, 177, 149, 153, 166, 176, 182, 191, 201, 208, 200, 165, 126, 90, 64, 47, 34, 23, 15, 12, 12, 15, 24, 41, 65, 96, 129, 158, 172, 168, 158, 147, 141, 140, 136, 116, 88, 63, 47, 40, 33}
+    Private TrafficDistribution() As Integer = {16, 10, 8, 10, 20, 50, 114, 182, 185, 152, 147, 150, 150, 150, 154, 165, 189, 195, 147, 96, 67, 51, 37, 23, 15, 11, 10, 11, 19, 45, 112, 188, 193, 154, 139, 139, 140, 144, 152, 169, 199, 204, 157, 100, 68, 51, 39, 25, 16, 11, 10, 11, 19, 44, 110, 189, 195, 156, 141, 141, 143, 147, 156, 173, 203, 209, 163, 107, 72, 55, 41, 27, 16, 12, 10, 12, 19, 44, 109, 188, 195, 157, 145, 146, 149, 154, 163, 180, 208, 212, 169, 117, 81, 60, 45, 29, 18, 13, 11, 13, 19, 42, 99, 171, 177, 149, 153, 166, 176, 182, 191, 201, 208, 200, 165, 126, 90, 64, 47, 34, 23, 15, 12, 12, 15, 24, 41, 65, 96, 129, 158, 172, 168, 158, 147, 141, 140, 136, 116, 88, 63, 47, 40, 33, 24, 15, 10, 9, 10, 15, 24, 37, 55, 90, 129, 153, 158, 152, 148, 152, 156, 147, 129, 108, 84, 61, 41, 26}
     Private Const TrafficDistributionMax As Integer = 212
 
     Private Const TRAFFIC_LIGHT_FREQ As Integer = 60
@@ -73,7 +73,7 @@
     End Function
 
     Function RandomDelayHelper(ByVal IDSeed As Long, ByVal Time As TimeSpan, ByVal Period As Integer, ByVal Length As Integer, ByVal Coefficient As Double) As Boolean
-        Dim HourOfWeek As Integer = TimeSpan.FromTicks(Time.Ticks Mod TimeSpan.FromDays(7).Ticks).TotalHours
+        Dim HourOfWeek As Integer = Int(TimeSpan.FromTicks(Time.Ticks Mod TimeSpan.FromDays(7).Ticks).TotalHours)
         Dim Seed As Long = (Time.TotalSeconds \ Period) + IDSeed
         Dim RNG As New Random(Seed Mod Integer.MaxValue)
 
@@ -90,10 +90,10 @@
 
     Public Function GetAverageDelayLength(ByVal Node As Node, ByVal Way As Way) As Double
         'By default use the delay expected on a weekday at noon.
-        Return GetAverageDelayLength(Node, Way, TimeSpan.FromHours(36))
+        Return GetAverageDelayLength(Node, Way, TimeSpan.FromHours(12))
     End Function
     Public Function GetAverageDelayLength(ByVal Node As Node, ByVal Way As Way, ByVal Time As TimeSpan) As Double
-        Dim HourOfWeek As Integer = TimeSpan.FromTicks(Time.Ticks Mod TimeSpan.FromDays(7).Ticks).TotalHours
+        Dim HourOfWeek As Integer = Int(TimeSpan.FromTicks(Time.Ticks Mod TimeSpan.FromDays(7).Ticks).TotalHours)
 
         If Way.Type = WayType.ROAD_MOTORWAY AndAlso Node.RoadDelay = RoadDelay.UNEXPECTED Then
             Return 0
