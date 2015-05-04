56a1ad7393661f34de5af8202ed860216bf8f6f2
jc4512 <jc4512@imperial.ac.uk>
2015-05-04 13:13:06 +0100

    Made mercator more complicated for no reason

--- AutomatedAgents/Graphics/CoordinateConverter.vb
+++ AutomatedAgents/Graphics/CoordinateConverter.vb
@@ -7,15 +7,16 @@
     Private LatHeight As Double
 
     Private MaxLat As Double
+    Private MinLat As Double
 
     Public Sub New(ByVal Bounds As Bounds, ByVal PanelWidth As Integer, ByVal PanelHeight As Integer)
         Me.Bounds = Bounds
         Me.PanelHeight = PanelHeight
         Me.PanelWidth = PanelWidth
         LongLength = Bounds.MaxLongitude - Bounds.MinLongitude
+        MinLat = 180.0 / Math.PI * Math.Log(Math.Tan(Math.PI / 4.0 + Bounds.MinLatitude * (System.Math.PI / 180.0) / 2))
         MaxLat = 180.0 / Math.PI * Math.Log(Math.Tan(Math.PI / 4.0 + Bounds.MaxLatitude * (System.Math.PI / 180.0) / 2))
-        LatHeight = MaxLat - _
-                    180.0 / Math.PI * Math.Log(Math.Tan(Math.PI / 4.0 + Bounds.MinLatitude * (System.Math.PI / 180.0) / 2))
+        LatHeight = MaxLat - MinLat
     End Sub
 
     Public Function GetPoint(ByVal RoutingPoint As IPoint) As Point
@@ -23,9 +24,11 @@
     End Function
 
     Public Function GetPoint(ByVal Latitude As Double, ByVal Longitude As Double) As Point
-        Dim X As Integer = (1 - ((Bounds.MaxLongitude - Longitude) / LongLength)) * PanelWidth
-        Dim TransformedLat As Double = 180.0 / Math.PI * Math.Log(Math.Tan(Math.PI / 4.0 + Latitude * (System.Math.PI / 180.0) / 2))
-        Dim Y As Integer = ((MaxLat - TransformedLat) / LatHeight) * PanelHeight
+        Dim TransformedLong As Double = Longitude - Bounds.MinLongitude
+        Dim X As Integer = (TransformedLong / LongLength) * PanelWidth
+        Dim TransformedLat As Double = 180.0 / Math.PI * Math.Log(Math.Tan(Math.PI / 4.0 + Latitude * (Math.PI / 180.0) / 2))
+        Dim Y As Integer = (1 - (TransformedLat - MinLat) / LatHeight) * PanelHeight
+
         Return New Point(X, Y)
     End Function
 
